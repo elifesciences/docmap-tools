beb78417f662892364e62befb654094891b994c3
Graham Nott <gnott@starglobal.ca>
2022-11-28 17:52:48 -0800

    Parse docmap and add logging.

--- docmaptools/__init__.py
+++ docmaptools/__init__.py
@@ -1,1 +1,19 @@
-__version__ = "0.2.0"
+import logging
+
+__version__ = "0.3.0"
+
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(logging.NullHandler())
+
+
+def configure_logging(filename, level=logging.INFO, format_string=None):
+    "configure logging to file"
+    if not format_string:
+        format_string = "%(levelname)s %(name)s:%(module)s:%(funcName)s: %(message)s"
+    handler = logging.FileHandler(filename)
+    formatter = logging.Formatter(format_string)
+    handler.setFormatter(formatter)
+    LOGGER.addHandler(handler)
+    LOGGER.setLevel(level)
+    return handler
